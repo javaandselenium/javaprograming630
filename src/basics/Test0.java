d927f2daf543003230b84102e08d31eb35a20fce
QSP <QSP@QSP>
2021-04-27 07:23:14 +0530

    Hi

---
+++ src/basics/Test0.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class Test0 {
+
+	public static void main(String[] args) {
+		System.out.println(true);
+		System.out.println(false);
+		System.out.println('S');
+		System.out.println(123);
+		System.out.println(23.900);
+		System.out.println("Java is a subject");
+
+	}
+
+}
---
+++ src/basics/Test1.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println(20+20);
+		System.out.println("The number is 20"+20);
+		System.out.println(20+20+"the number is");
+		System.out.println(20+(20+20));
+
+	}
+
+}
